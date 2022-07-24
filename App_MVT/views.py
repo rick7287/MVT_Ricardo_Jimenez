e7849ffa85dced0e464008fd6f55ed91399908cc
RicJimenez <ricardojimfra@gmail.com>
2022-07-24 15:57:10 -0500

    Save con dato tipo Date funciona

--- App_MVT/views.py
+++ App_MVT/views.py
@@ -9,31 +9,37 @@ from App_MVT.models import Familiar
 
 def familiar(self):
 
-    persona1 = Persona(nombre = "Isaac Jimenez", fecha_nac = "5/12/1985", edad = 37)
-    persona1.save()
+    #persona1 = Persona(nombre = "Isaac Jimenez", fecha_nac = "5/12/1985", edad = 37)
+    #persona1.save()
 
-    persona2 = Persona(nombre = "Ricardo Jimenez", fecha_nac = "7/2/1987", edad = 35)
-    persona2.save()
-
-
-    persona3 = Persona(nombre = "Pedro Jimenez", fecha_nac = "5/4/1988", edad = 34)
-    persona3.save()
+    #persona2 = Persona(nombre = "Ricardo Jimenez", fecha_nac = "7/2/1987", edad = 35)
+    #persona2.save()
 
 
-    #familiar1 = Familiar(nombre = "Pedro Jimenez", fecha_nac = 1988/4/5, edad = 34)
-    #familiar1.save()
+    #persona3 = Persona(nombre = "Pedro Jimenez", fecha_nac = "5/4/1988", edad = 34)
+    #persona3.save()
 
-    nombre1 = persona1.nombre
-    fecha1 = persona1.fecha_nac
-    edad1 = persona1.edad
 
-    nombre2 = persona2.nombre
-    fecha2 = persona2.fecha_nac
-    edad2 = persona2.edad
+    familiar1 = Familiar(nombre = "Pedro Jimenez", fecha_nac = '1988-4-5', edad = 34)
+    familiar1.save()
 
-    nombre3 = persona3.nombre
-    fecha3 = persona3.fecha_nac
-    edad3 = persona3.edad
+    familiar2 = Familiar(nombre = "Ricardo Jimenez", fecha_nac = '1987-2-7', edad = 35)
+    familiar2.save()
+
+    familiar3 = Familiar(nombre = "Isaac Jimenez", fecha_nac = '1984-12-5', edad = 37)
+    familiar3.save()
+
+    nombre1 = familiar1.nombre
+    fecha1 = familiar1.fecha_nac
+    edad1 = familiar1.edad
+
+    nombre2 = familiar2.nombre
+    fecha2 = familiar2.fecha_nac
+    edad2 = familiar2.edad
+
+    nombre3 = familiar3.nombre
+    fecha3 = familiar3.fecha_nac
+    edad3 = familiar3.edad
 
     dicc = {'nombre1':nombre1, 
             'fecha1':fecha1, 
@@ -52,7 +58,7 @@ def familiar(self):
     documento = plantilla.render(dicc)  
     return HttpResponse(documento)
 
-    #texto = f"Registro creado: {persona1.nombre} {persona1.fecha_nac} {persona1.edad}"
+    #texto = f"Registro creado: {familiar1.nombre} {familiar1.fecha_nac} {familiar1.edad}"
     #return HttpResponse(texto)
 
     
